2f28b7dffb3729e7f90d3b3a620a1c388466c498
martinamineo1998 <martina.mineo.1998@gmail.com>
2019-05-14 17:19:45 +0200

    scroll.js updated and probably finished

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,1 +1,9 @@
-let scrollElement = document.getElementsByClassName('header__scroll-down')
+let scrollDownElement = document.querySelector('.header__scroll-down')
+    scrollDownElement.addEventListener('click', function() {
+      window.scrollBy(0, 100 * window.innerHeight/100)
+    })
+
+let scrollTopElement = document.querySelector('.footer__scroll-top')
+    scrollTopElement.addEventListener('click', function() {
+      window.scrollBy(0, -100 * window.innerHeight/100)
+    })
